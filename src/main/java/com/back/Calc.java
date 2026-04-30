3ed0bb5a583365a7507050eefd6875d422a4c3fd
bighead0831 <bighead0831@naver.com>
2026-04-30 22:41:19 +0900

    completed  Red, Green, Blue

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -3,8 +3,13 @@ package com.back;
 public class Calc {
 
     public static int run(String exp) {
-        String[] n = exp.trim().split(" \\+ ");
-
-        return Integer.parseInt(n[0])+Integer.parseInt(n[1]);
+        if(exp.contains("+")) {
+            String[] n = exp.trim().split(" \\+ ");
+            return Integer.parseInt(n[0])+Integer.parseInt(n[1]);
+        } else if(exp.contains("-")) {
+            String[] n = exp.trim().split(" - ");
+            return Integer.parseInt(n[0])-Integer.parseInt(n[1]);
+        }
+        return 0;
     }
 }
