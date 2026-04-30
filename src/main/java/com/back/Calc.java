c5124434721f9045cce77306cbec06da935cadf6
bighead0831 <bighead0831@naver.com>
2026-04-30 22:39:09 +0900

    completed  Red, Green, Blue

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -3,6 +3,8 @@ package com.back;
 public class Calc {
 
     public static int run(String exp) {
-        return 2;
+        String[] n = exp.trim().split(" \\+ ");
+
+        return Integer.parseInt(n[0])+Integer.parseInt(n[1]);
     }
 }
