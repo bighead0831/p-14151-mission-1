b996b458010f042a34204ee73f50bac47212dc78
bighead0831 <bighead0831@naver.com>
2026-04-30 23:26:49 +0900

    completed to t1 from t14 Red, Green, Blue

--- src/test/java/com/back/CalcTest.java
+++ src/test/java/com/back/CalcTest.java
@@ -89,7 +89,7 @@ public class CalcTest {
     void t14() {
         assertThat(Calc.run("10 * 10 * 10")).isEqualTo(1000);
     }
-
+//
 //    @Test
 //    @DisplayName("10 + 5 * 2 == 20")
 //    void t15() {
